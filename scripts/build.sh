a471e4add26275a26e242ad0eff7e04ba9bc1d25
oitsjustjose <stovallj1995@gmail.com>
2020-07-08 10:02:31 -0500

    Clean up build

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,18 +8,14 @@ tsc
 
 echo "Building for Windows"
 
-electron-builder . --win
-
-echo "Building for macOS"
-
-electron-builder . --mac
+electron-builder --win
 
 echo "Building for Linux"
 
-electron-builder . --linux
+electron-builder --linux
 
-electron-builder . --linux snap
+electron-builder --linux snap
 
-# snapcraft push --release=stable dist/Gmail-Desktop_*.snap
-
-echo "Done!"
+echo "Done building, remember to push to snapcraft as well as build for macOS"
+echo "Build for macOS using 'npx electron-builder --mac'"
+echo "Push to SnapCraft using 'snapcraft push --release=stable dist/Gmail-Desktop_*.snap'"
